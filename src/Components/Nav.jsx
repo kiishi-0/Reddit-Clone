165f988522d68122753ca30891cbe63eccb1abab
kiishi-0 <kiishioluwa@gmail.com>
2022-08-16 22:41:10 +0100

    Started development of the reddit clone

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import logo from '../images/reddit-logo.svg'
+import SearchBar from './SearchBar'
 
 const NavBar = styled.nav`
     width:100%;
@@ -20,13 +21,39 @@ const Logo = styled.img`
     width: 100%;
     object-fit: contain;
 `
-
+const LoginButton = styled.a`
+    height: 30px;
+    width: 120px;
+    border: 1px solid #0487D9;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    // padding: 0 0 2px;
+    border-radius: 100px;
+    margin: 0 10px;
+    color: #0487D9;
+    font-weight: 600;
+    :hover{
+        background: rgba(0,121,211,0.05);
+    }
+`
+const SignUpButton = styled(LoginButton)`
+    background: #0487D9;
+    color: #FFFFFF;
+    :hover{
+        background: #0094f0;
+        border-color: #0094f0;
+    }
+`
 export default function Nav() {
   return (
     <NavBar>
         <LogoContainer>
             <Logo src={logo}/>
         </LogoContainer>
+        <SearchBar></SearchBar>
+        <LoginButton>Log In</LoginButton>
+        <SignUpButton>Sign Up</SignUpButton>
     </NavBar>
   )
 }
---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const SearchBarContainer = styled.div`
+    width: 60%;
+    margin: 0 20px;
+`
+const SearchBarForm = styled.form`
+    width: 100%;
+    display: flex;
+    background-color: #F6F7F9;
+    height: 30px;
+    border: 1px solid #EDEFF1;
+`
+const SearchInput = styled.input`
+    width: 80%;
+    height: 100%;
+    border: 0;
+    background: transparent;
+    padding: 2px 0;
+    &:focus{
+        border: none;
+        // border: 1px solid #0487D9;
+    }
+`
+
+export default function SearchBar() {
+  return (
+    <SearchBarContainer>
+        <SearchBarForm>
+            <SearchInput type="text"n placeholder='Search Reddit'/>
+        </SearchBarForm>
+    </SearchBarContainer>
+  )
+}
